5870f1fa3a3dcf0c3698d3e0622d2b47ebdff522
xiaohe10 <xiaoh16@gmail.com>
2014-01-05 16:18:57 +0800

    增加注册错误提示

--- expertRecommendation/web/account_views.py
+++ expertRecommendation/web/account_views.py
@@ -16,11 +16,23 @@ def register(request):
         validcode = request.POST['validcode']
         if(validcode != 'zhuanjiaku'):
             message = '验证码错误，应该是 zhuanjiaku'
+            form = UserCreationForm(request.POST)
+            return render(request,"account/register.html", {'form': form,'message':message})
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
             return HttpResponseRedirect("/")
-        user = auth.get_user_model()
+        try:
+            user = User.objects.get(username = request.POST['username'])
+            message = "该用户名已经存在"
+        except:
+            pass1 = request.POST["password1"]
+            pass2 = request.POST["password2"]
+            if pass1 != pass2:
+                message = "密码输入不一致"
+            elif  pass1 == '':
+                message = "密码不能为空"
+
     else:
         form = UserCreationForm()
     return render(request,"account/register.html", {
